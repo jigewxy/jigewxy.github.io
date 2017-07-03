a909f3b2dd5cdea2be2125f837b02ec6f2ad3676
Wang Cliff <xi-yu-cliff.wang@hp.com>
2017-07-03 18:57:11 +0800

    Build the skeleton with angularjs

---
+++ js/app.js
@@ -0,0 +1,46 @@
+var app = angular.module('myApp',["ngRoute"]);
+
+app.config(function($routeProvider){
+    $routeProvider
+    .when("/resume", {
+      templateUrl: "resume.html",
+      controller: "resumeCtrl"
+    })
+    .when("/webdev",{
+       templateUrl:"webdev.html",
+       controller: "webDevCtrl"
+    })
+    .when("/embed", {
+        templateUrl:"embed.html",
+        controller: "embedCtrl"
+    })
+    .when("/misc",{
+        templateUrl:"misc.html",
+        controller:"miscCtrl"})
+    .otherwise({redirectTo: "/webdev"})
+ });
+
+
+
+ app.controller("resumeCtrl", function($scope){
+  
+    console.log("resumeCtrl");
+
+ });
+
+ app.controller("webDevCtrl", function($scope){
+
+    console.log("webdevCtrl");
+
+ });
+
+app.controller("embedCtrl", function($scope){
+
+    console.log("embedCtrl");
+
+});
+
+app.controller("misc", function($scope){
+    
+  console.log("miscCtrl");
+});
